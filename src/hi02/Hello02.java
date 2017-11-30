78871522001ab524b1d5092f8ced3d7217fb8b45
stillmind <iamready@yeah.net>
2017-11-30 09:26:49 +0800

    2

--- src/hi02/Hello02.java
+++ src/hi02/Hello02.java
@@ -3,5 +3,7 @@ package hi02;
 public class Hello02 {
 	public static void main(String[] args) {
 		System.out.println("hello 02");
+		System.out.println("hello 11-30");
+		System.out.println("hello now");
 	}
 }
